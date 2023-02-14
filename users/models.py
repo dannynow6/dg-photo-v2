8975f1ddc69c6dd3c9842988bef0129bfd6ace0f
Dan Garro <dobetterwithdan@gmail.com>
2023-02-13 19:55:50 -0500

    added profile with onetoone to user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Profile(models.Model):
+    """A profile model to extend Django's build in User model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    email = models.EmailField(max_length=300, unique=True) 
+    
+    def __str__(self):
+        first = self.first_name 
+        last = self.last_name 
+        email = self.email 
+        return f"{first.title()} {last.title()} | {email}"
+    
+    
--- users/views.py
+++ users/views.py
@@ -20,3 +20,20 @@ def register(request):
     # display a blank or invalid form
     context = {"form": form}
     return render(request, "registration/register.html", context)
+
+    """
+    from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+    """
